66423f26d5df3ab3213d6ad423aa520af22afd64
Ilia Choly <ilia.choly@gmail.com>
2024-11-13 08:46:17 -0500

    scaffold out id order tests

--- src/stream.zig
+++ src/stream.zig
@@ -6,6 +6,13 @@ const StreamID = struct {
     timestamp: u64,
     sequence: u64,
 
+    fn decode(encoded: [16]u8) StreamID {
+        return .{
+            .timestamp = std.mem.readInt(u64, encoded[0..8], .big),
+            .sequence = std.mem.readInt(u64, encoded[8..], .big),
+        };
+    }
+
     fn encode(self: StreamID) [16]u8 {
         var encoded: [16]u8 = undefined;
         std.mem.writeInt(u64, encoded[0..8], self.timestamp, .big);
@@ -14,6 +21,22 @@ const StreamID = struct {
     }
 };
 
-test "StreamID.encode" {
+test "StreamID.encode: zero" {
     try testing.expectEqual([1]u8{0} ** 16, StreamID.encode(.{ .timestamp = 0, .sequence = 0 }));
+}
+
+test "StreamID.encode: order" {
+    const Test = struct {
+        lo: StreamID,
+        hi: StreamID,
+    };
+    const tests = [_]Test{.{
+        .lo = .{ .timestamp = 0, .sequence = 0 },
+        .hi = .{ .timestamp = 1, .sequence = 1 },
+    }};
+    for (tests) |t| {
+        const lo = t.lo.encode();
+        const hi = t.hi.encode();
+        try testing.expectEqual(std.mem.order(u8, &lo, &hi), .lt);
+    }
 }
